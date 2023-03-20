8dbbe6203f1060dcd4d351c06357021d6cd1aa5a
Saloni-tiwary <121880445+Saloni-tiwary@users.noreply.github.com>
2023-03-20 11:29:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-var http = require("http");
- http.createServer(function (request, response) {
-    const body = 'Hello World';
-response
-  .writeHead(200, { 
-    'Content-Length': Buffer.byteLength(body),
-    'Content-Type': 'text/plain'
-  })
-  .end(body);
- }).listen(8000);
- console.log('Server running at http://127.0.0.1:8000/');
+console.log("hello world")
