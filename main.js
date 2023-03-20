71b2bd38c3efdff463332f3c783c3ea3eca1ae79
Saloni-tiwary <121880445+Saloni-tiwary@users.noreply.github.com>
2023-03-20 14:29:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://127.0.0.1:${PORT}`);
+});
